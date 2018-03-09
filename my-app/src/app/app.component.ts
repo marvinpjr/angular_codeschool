2f9347830a6fd019e5ff6a3efc6e867904ca0860
marvinpjr <marvin@marvinpalmer.com>
2018-03-08 21:16:09 -0600

    created app

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Ultra Racing';
+  carPart = {
+    "id": 1,
+    "name": "Super Tires",
+    "description": "These tires are the very best",
+    "inStock": 5
+  };
+}
