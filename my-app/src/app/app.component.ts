135be279544627633385ee6b9e23a27deb39b93d
marvinpjr <marvin@marvinpalmer.com>
2018-03-08 21:33:09 -0600

    adding ngFor and ngIf structural directives

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -7,10 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ultra Racing';
-  carPart = {
+  carParts = [{
     "id": 1,
     "name": "Super Tires",
     "description": "These tires are the very best",
     "inStock": 5
-  };
+  },{
+    "id": 2,
+    "name": "Reinforced Shocks",
+    "description": "Shocks made by kryptonite",
+    "inStock": 0
+  },{
+    "id": 3,
+    "name": "Sparky Plugs",
+    "description": "Extra sparks in dem plugs",
+    "inStock": 7
+  }];
 }
