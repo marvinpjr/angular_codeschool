1df312f3b39c5f713e887d266c23cac6a07dd569
marvinpjr <marvin@marvinpalmer.com>
2018-03-11 18:47:31 -0500

    put data in a mock class

--- my-app/src/app/car-parts/car-parts.component.ts
+++ my-app/src/app/car-parts/car-parts.component.ts
@@ -1,36 +1,24 @@
 import {Component} from '@angular/core';
+import { CarPart } from './car-part';
+import { CARPARTS } from './mocks'
 
 @Component({
     selector: 'car-parts',
     templateUrl: './car-parts.component.html',
     styleUrls: ['./car-parts.component.css']
 })
-export class CarPartsComponent {
-    carParts = [{
-        "id": 1,
-        "name": "Super Tires",
-        "description": "These tires are the very best",
-        "price": 4.99,
-        "inStock": 5
-      },{
-        "id": 2,
-        "name": "Reinforced Shocks",
-        "description": "Shocks made by kryptonite",
-        "price": 2.99,
-        "inStock": 0
-      },{
-        "id": 3,
-        "name": "Sparky Plugs",
-        "description": "Extra sparks in dem plugs",
-        "price": 5.99,
-        "inStock": 7
-      }];
-    
-      totalCarParts(){ 
-        let sum = 0;
-        for (let carPart of this.carParts){
-          sum += carPart.inStock;
-        }
-        return sum;
-      }
+export class CarPartsComponent {  
+    carParts: CarPart[];
+
+    ngOnInit(){
+        this.carParts = CARPARTS;
+    }
+
+    totalCarParts(){ 
+    let sum = 0;
+    for (let carPart of this.carParts){
+        sum += carPart.inStock;
+    }
+    return sum;
+    }
 }
---
+++ my-app/src/app/car-parts/mocks.ts
@@ -0,0 +1,21 @@
+import { CarPart } from './car-part'
+
+export const CARPARTS: CarPart[] = [{
+    "id": 1,
+    "name": "Super Tires",
+    "description": "These tires are the very best",
+    "price": 4.99,
+    "inStock": 5
+  },{
+    "id": 2,
+    "name": "Reinforced Shocks",
+    "description": "Shocks made by kryptonite",
+    "price": 2.99,
+    "inStock": 0
+  },{
+    "id": 3,
+    "name": "Sparky Plugs",
+    "description": "Extra sparks in dem plugs",
+    "price": 5.99,
+    "inStock": 7
+  }];
